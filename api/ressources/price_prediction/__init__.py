d5270ba7c6a90030780c31022316ef989b428e1d
martin_cudicio <martin.cudicio@saegus.com>
2021-04-01 11:47:28 +0200

    add log training dataset
    save training dataset with mlflow

--- api/ressources/price_prediction/__init__.py
+++ api/ressources/price_prediction/__init__.py
@@ -3,17 +3,15 @@
 ## De ce fait, le modèle sera en RAM et n'aura pas besoin d'être chargé à chaque appel
 
 ## EXEMPLE
-# import pandas as pd
-# from sklearn.externals import joblib
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.preprocessing import LabelEncoder,OneHotEncoder
-# from sklearn.decomposition import TruncatedSVD
-# from sklearn.ensemble import RandomForestClassifier
+# import mlflow.sklearn
 
 import mlflow.sklearn
 import os
 cwd = os.getcwd()
-# display currend directory
+
+# afficher le répertoire courant
 print(cwd)
-sk_model = mlflow.sklearn.load_model("../train/mlruns/0/96b71f5653f9494496a63bbd54628877/artifacts/model")
-# StatAuteurPASSE = pd.read_pickle('resources/ressoc/ComptageAuteur.pkl')
+
+# inserer l'ID du modèle à utiliser
+model_id = "08e4968c7bfe405c8234504387cb8e8d"
+sk_model = mlflow.sklearn.load_model("../train/mlruns/0/"+model_id+"/artifacts/model")
